0d4b6b0b03be51febf2de84e03299b8622d00475
svaronc <sebastianvaron96@gmail.com>
2024-03-24 12:30:23 -0500

    fix with issue

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,6 @@ const Projects = () => {
           <ProjectCard info={info} key={info.id}/>
         ))}
         <div className="absolute z-[0] w-[40%] h-[35%] pink__gradient left-[-680px]" />
-          <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient right-[-1366px]" />
       </div>
     </section>
   );
